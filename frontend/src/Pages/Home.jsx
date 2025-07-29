b3c80d9e2cec7a6fe5f96fc564c07f704d94fa68
5728 - Sai Kishor <5728sak@manorama.net>
2025-07-29 12:32:21 +0530

    sd

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -8,6 +8,7 @@ function Home() {
       <h3>Offended aaayi njaaan</h3>
       <h1>Ningada project njn deelet cheyum</h1>
       <h1>Hi iam gopz</h1>
+      <h1>Arya offedne</h1>
      
       <h1>aaaabllin</h1>
     </div>
